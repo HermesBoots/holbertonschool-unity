da27def986bfde90167161f1bac6bcef3a27837a
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-03-24 02:10:19 -0400

    Add task 9: add victory condition

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>Handles victory conditions.</summary>
+public class WinTrigger : MonoBehaviour
+{
+    /// <summary>Stop the timer when the player hits the end.</summary>
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log(other.name);
+        if (other.name.Equals("Player"))
+            other.GetComponent<Timer>().Stop();
+    }
+}
