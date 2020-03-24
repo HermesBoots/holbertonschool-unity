e9bc748eeb9ab72aefd2b7b570fdca13b8c06421
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-03-24 02:00:35 -0400

    Add task 8: trigger time when player moves

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>Manipulates the on-screen timer.</summary>
+public class Timer : MonoBehaviour
+{
+    /// <summary>The text UI element to update.</summary>
+    public Text timerText;
+    
+    /// <summary>Update the timer each frame.</summary>
+    void Update()
+    {
+        this.timerText.text = string.Format("{0}:{1:00.00}", Mathf.RoundToInt(Time.time / 60), Time.time % 60);
+    }
+}
