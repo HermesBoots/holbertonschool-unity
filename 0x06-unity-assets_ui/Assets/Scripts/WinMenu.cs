26972d95d90a97cf5f02389f9315b0af352f638d
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-04-07 08:17:41 -0400

    Add task 9: go to next level after winning

---
+++ 0x06-unity-assets_ui/Assets/Scripts/WinMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+/// <summary>Contains callbacks for the victory screen.</summary>
+public class WinMenu : MonoBehaviour
+{
+    /// <summary>Return to the main menu.</summary>
+    public void MainMenu() {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    /// <summary>Go to the next level.</summary>
+    public void Next() {
+        int number;
+
+        number = Int32.Parse(SceneManager.GetActiveScene().name.Substring(5));
+        if (number < 3)
+            SceneManager.LoadScene(String.Format("Level{0:D2}", number + 1));
+        else
+            SceneManager.LoadScene("MainMenu");
+    }
+}
--- 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 /// <summary>Handles victory conditions.</summary>
 public class WinTrigger : MonoBehaviour
 {
-    /// <summary>Stop the timer when the player hits the end.</summary>
+    /// <summary>Victory UI.</summary>
+    public Canvas winScreen;
+
+    // Stop the timer when the player hits the end.
     private void OnTriggerEnter(Collider other) {
-        if (other.name.Equals("Player"))
-            other.GetComponent<Timer>().Stop();
+        if (other.name.Equals("Player")) {
+            this.winScreen.gameObject.SetActive(true);
+            GameObject.Find("Player").GetComponent<Timer>().Win();
+        }
     }
 }
