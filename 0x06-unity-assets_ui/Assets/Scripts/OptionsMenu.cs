661408591cf8861ae26d5c878fc23ad734371cbe
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-04-07 05:30:46 -0400

    Add task 3: navigating between menus

---
+++ 0x06-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+/// <summary>Contains operations performed in the options menu.</summary>
+public class OptionsMenu : MonoBehaviour
+{
+    // name of last scene loaded
+    private static string lastScene;
+
+    /// <summary>Load a new scene and remember the previous one.</summary>
+    /// <param name="name">Which scene to load next.</param>
+    public static void MyLoadScene(string name) {
+        OptionsMenu.lastScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(name);
+    }
+
+    /// <summary>Return to the previous scene.</summary>
+    public void Back() {
+        SceneManager.LoadScene(OptionsMenu.lastScene);
+    }
+}
