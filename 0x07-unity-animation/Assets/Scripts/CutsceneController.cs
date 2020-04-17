3b25137512750bde2bd180d9b41f485d291b9f3f
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-04-16 20:47:56 -0400

    Add task 2: transition between cameras

---
+++ 0x07-unity-animation/Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>Controls the camera animations and transitions back to gameplay.</summary>
+public class CutsceneController : MonoBehaviour
+{
+    /// <summary>The player object to toggle.</summary>
+    public PlayerController player;
+    /// <summary>The main camera to toggle.</summary>
+    public Camera myCamera;
+    /// <summary>The timer to toggle.</summary>
+    public Canvas timer;
+
+    /// <summary>Return control to the game when the cutscene ends.</summary>
+    public void Finished() {
+        this.player.enabled = true;
+        this.myCamera.gameObject.SetActive(true);
+        this.timer.gameObject.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}
