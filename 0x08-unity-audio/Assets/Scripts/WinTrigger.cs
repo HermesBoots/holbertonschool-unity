77f0dfc3b8619fd313bce95e447da85b6e10a68f
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-05-04 09:42:17 -0400

    Add task 6: sound when beating level

--- 0x08-unity-audio/Assets/Scripts/WinTrigger.cs
+++ 0x08-unity-audio/Assets/Scripts/WinTrigger.cs
@@ -10,6 +10,9 @@ public class WinTrigger : MonoBehaviour
     /// <summary>Victory UI.</summary>
     public Canvas winScreen;
 
+    /// <summary>Sound that plays when beating the level.</summary>
+    public AudioClip winSound;
+
     // Stop the timer when the player hits the end.
     private void OnTriggerEnter(Collider other) {
         if (other.name.Equals("Player")) {
@@ -17,5 +20,7 @@ public class WinTrigger : MonoBehaviour
             GameObject.Find("Player").GetComponent<Timer>().Win();
         }
         GameObject.Find("Main Camera").GetComponent<AudioSource>().Stop();
+        GameObject.Find("Main Camera").GetComponent<AudioSource>().clip = this.winSound;
+        GameObject.Find("Main Camera").GetComponent<AudioSource>().Play();
     }
 }
