25fd99d6a932b23bfcb30432d60831080826f8a9
HermesBoots <Samuel.of.Hermes@GMail.com>
2020-05-04 10:25:17 -0400

    Add tasks 9 and 10: support volume options

--- 0x08-unity-audio/Assets/Scripts/MainMenu.cs
+++ 0x08-unity-audio/Assets/Scripts/MainMenu.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Audio;
 
 
 /// <summary>Contains button callbacks for the main menu.</summary>
 public class MainMenu : MonoBehaviour
 {
+    /// <summary>Mixer to adjust volume of.</summary>
+    public AudioMixer mixer;
+
     /// <summary>Quit the game.</summary>
     public void Exit() {
         Debug.Log("Exited");
@@ -20,5 +24,16 @@ public class MainMenu : MonoBehaviour
     /// <summary>Load the options menu.</summary>
     public void Options() {
         OptionsMenu.MyLoadScene("Options");
+    }
+
+    // Adjust volume when starting the game
+    private void Start() {
+        if (PlayerPrefs.HasKey("BGMVol"))
+            this.mixer.SetFloat("BGMVol", PlayerPrefs.GetFloat("BGMVol") != 0 ? 20 * Mathf.Log10(PlayerPrefs.GetFloat("BGMVol")) : -144);
+        if (PlayerPrefs.HasKey("SFXVol")) {
+            this.mixer.SetFloat("RunningVol", PlayerPrefs.GetFloat("SFXVol") != 0 ? 20 * Mathf.Log10(PlayerPrefs.GetFloat("SFXVol")) : -144);
+            this.mixer.SetFloat("LandingVol", PlayerPrefs.GetFloat("SFXVol") != 0 ? 20 * Mathf.Log10(PlayerPrefs.GetFloat("SFXVol")) : -144);
+            this.mixer.SetFloat("AmbientVol", PlayerPrefs.GetFloat("SFXVol") != 0 ? 20 * Mathf.Log10(PlayerPrefs.GetFloat("SFXVol")) : -144);
+        }
     }
 }
--- 0x08-unity-audio/Assets/Scripts/OptionsMenu.cs
+++ 0x08-unity-audio/Assets/Scripts/OptionsMenu.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -6,6 +7,9 @@ using UnityEngine.UI;
 /// <summary>Contains operations performed in the options menu.</summary>
 public class OptionsMenu : MonoBehaviour
 {
+    /// <summary>BGM audio group to adjust.</summary>
+    public AudioMixer mixer;
+
     // name of last scene loaded
     private static string lastScene;
 
@@ -19,6 +23,12 @@ public class OptionsMenu : MonoBehaviour
     /// <summary>Save and apply current options.</summary>
     public void Apply() {
         PlayerPrefs.SetInt("InvertY", this.GetComponentInChildren<Toggle>().isOn ? 1 : 0);
+        PlayerPrefs.SetFloat("BGMVol", GameObject.Find("BGMSlider").GetComponent<Slider>().value);
+        PlayerPrefs.SetFloat("SFXVol", GameObject.Find("SFXSlider").GetComponent<Slider>().value);
+        this.mixer.SetFloat("BGMVol", PlayerPrefs.GetFloat("BGMVol") != 0 ? 20 * Mathf.Log10(PlayerPrefs.GetFloat("BGMVol")) : -144);
+        this.mixer.SetFloat("RunningVol", PlayerPrefs.GetFloat("SFXVol") != 0 ? 20 * Mathf.Log10(PlayerPrefs.GetFloat("SFXVol")) : -144);
+        this.mixer.SetFloat("LandingVol", PlayerPrefs.GetFloat("SFXVol") != 0 ? 20 * Mathf.Log10(PlayerPrefs.GetFloat("SFXVol")) : -144);
+        this.mixer.SetFloat("AmbientVol", PlayerPrefs.GetFloat("SFXVol") != 0 ? 20 * Mathf.Log10(PlayerPrefs.GetFloat("SFXVol")) : -144);
     }
 
     /// <summary>Return to the previous scene.</summary>
@@ -32,5 +42,13 @@ public class OptionsMenu : MonoBehaviour
             this.GetComponentInChildren<Toggle>().isOn = PlayerPrefs.GetInt("InvertY") == 0 ? false : true;
         else
             this.GetComponentInChildren<Toggle>().isOn = false;
+        if (PlayerPrefs.HasKey("BGMVol"))
+            GameObject.Find("BGMSlider").GetComponent<Slider>().value = PlayerPrefs.GetFloat("BGMVol");
+        else
+            GameObject.Find("BGMSlider").GetComponent<Slider>().value = 1;
+        if (PlayerPrefs.HasKey("SFXVol"))
+            GameObject.Find("SFXSlider").GetComponent<Slider>().value = PlayerPrefs.GetFloat("SFXVol");
+        else
+            GameObject.Find("SFXSlider").GetComponent<Slider>().value = 1;
     }
 }
